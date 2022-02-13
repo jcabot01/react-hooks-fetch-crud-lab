88ee28477949e372422cc6449f8624037ba35c87
JonCabot <jfc0053@gmail.com>
2022-02-13 15:26:59 -0600

    completed assignment

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -19,8 +19,27 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
-  }
+    // console.log(formData);
+    //POST
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: formData.prompt,
+        answers: [
+          formData.answer1,
+          formData.answer2,
+          formData.answer3,
+          formData.answer4,
+        ],
+        correctIndex: parseInt(formData.correctIndex),
+      }),
+    });
+  }  //this displays on QuestionList without any extra code, because this POSTs to the db, then the GET grabs it when we reload page to navigate back to list.
+
+
 
   return (
     <section>
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [isQuestions, setIsQuestions] = useState([])
+
+  // useEffect/fetch GET.  Set State to incoming JSON data.
+  useEffect(()=> {
+    fetch("http://localhost:4000/questions") 
+      .then((r) => r.json())
+      .then((questions) => {
+        setIsQuestions(questions)
+      })
+  }, []);
+
+  function deleteQuestionButton(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+    .then((r) => r.json())
+    .then(() => {
+      const updatedQuestions = isQuestions.filter((question) => question.id !== id)
+      setIsQuestions(updatedQuestions);
+    });
+  }
+  
+  function updateAnswerChange(id, correctIndex) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex }),  //pass in correctIndex as the part to be updated
+    })
+      .then((r) => r.json())
+      .then((updatedQuestion) => {
+        const updatedQuestions = isQuestions.map((question) => {
+          if(question.id === updatedQuestion.id){
+            return updatedQuestion;
+          } else {
+          return question;
+          }
+        });
+        setIsQuestions(updatedQuestions);
+      });
+  }
+
+
+  //set variable containing <QuestionItem/> component, map over the current State of questions to render to page. 
+  const questionItems = isQuestions.map((question) => (
+    <QuestionItem 
+      key = {question.id}
+      question = {question}
+      onDeleteClick = {deleteQuestionButton}
+      onChange = {updateAnswerChange}
+    />
+  ));
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionItems}</ul>
     </section>
   );
 }
